51b817653bc21161e5111784b413c5870e8ae6b4
ttyson0311 <ttyson0311@gmail.com>
2019-04-27 17:32:31 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// document.querySelector("button").onclick = () => {
+//   location.href = "./project/project.html";
+// };
+
+// function scrollIntoView() {
+//   window.scrollBy(100, 0);
+// }
+
+document.querySelector(".hello").scrollIntoView({
+  behavior: "smooth"
+});
+
+// window.scroll({
+//   top: 2500,
+//   left: 0,
+//   behavior: 'smooth' <pre rel="HTML"><code markup="tt" class="language-markup">
+// })
